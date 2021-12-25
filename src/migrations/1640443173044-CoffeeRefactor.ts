540743c45189ccfbf2b2a964419b2f6e85022190
Raphael Astrow <raphaelastrow@zoho.com>
2021-12-25 10:48:29 -0500

    Chapter 3

---
+++ src/migrations/1640443173044-CoffeeRefactor.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { query } from 'express';
+
+export class CoffeeRefactor1640443173044 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coffee" RENAME COLUMN "name" TO "title"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coffee" RENAME COLUMN "title" TO "name"`,
+    );
+  }
+}
